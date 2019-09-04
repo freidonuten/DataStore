283707a8de7f56f68012abfdaad9f056e278dd34
Martin Procházka <martin.prochazka@protonmail.com>
2019-09-04 15:47:33 +0200

    major refactor events are fired on each write call instead of storing the data in internal mapping

--- contracts/DataStore.sol
+++ contracts/DataStore.sol
@@ -3,33 +3,14 @@ pragma solidity >=0.4.21 <0.6.0;
 contract DataStore {
 
     address public owner;
-    mapping (bytes32 => Data) public dataMapping;
-
-    struct Data {
-        string content;
-        uint timestamp;
-    }
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function read(bytes32 hash) public view returns (string memory, uint) {
-        Data memory output = dataMapping[hash];
+    event OnWrite(string data);
 
-        return (output.content, output.timestamp);
+    function write(string memory data) public {
+        emit OnWrite(data);
     }
-
-    function write(string memory content) public {
-        bytes32 hash = keccak256(abi.encodePacked(content, now));
-
-        require(!exists(hash), "Message already exists.");
-
-        dataMapping[hash] = Data(content, now);
-    }
-
-    function exists(bytes32 hash) public view returns (bool) {
-        return dataMapping[hash].timestamp != 0;
-    }
-
 }
